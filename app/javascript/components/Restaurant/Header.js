c01675a9d57a059e47d70c6870302809e47b6cf8
sanadwj <s_wj_88@hotmail.com>
2021-01-25 03:26:16 +0200

    Add restaurants component
    Add restaurant component
    
    Add styles

---
+++ app/javascript/components/Restaurant/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = (props) => {
+    return (
+        <div className="wrapper">
+            <h1>
+                <img src="" alt=""/>
+                Restaurant Name
+            </h1>
+        </div>
+    )
+}
--- app/javascript/components/Restaurant/Restaurant.js
+++ app/javascript/components/Restaurant/Restaurant.js
@@ -1,8 +1,27 @@
 import React, { useState, useEffect } from 'react'
+import axios from "axios";
 
-const Restaurant =() => {
+const Restaurant = (props) => {
+    const [restaurant,setRestaurant] = useState({})
+    const [review,setReview] = useState({})
+
+    useEffect(() => {
+        const slug = props.match.params.slug
+        const url =`/api/v1/restaurants/${slug}`
+        axios.get(url)
+            .then(res => setRestaurant(res.data))
+            .catch(res => console.log(res))
+    }, [])
     return (
-        <div>This is the Restaurant#show view for the app.</div>
+        <div className="wrapper">
+            <div className="column">
+                <div className="header"></div>
+                <div className="reviews"></div>
+            </div>
+            <div className="column">
+                <div className="reviewForm">[Review form goes here]</div>
+            </div>
+        </div>
     )
 }
 
